70f7057a1b8dc9f0fcf85df37da34fa407bf38a6
MD. IBRAHIM KHALIL <58024873+ibrahimk4111@users.noreply.github.com>
2022-04-03 13:14:56 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Users> </Users>
+    </div>
+  );
+}
+
+function Users() {
+  //using UseState Hook
+  const [counter, setCounter] = useState(55);
+
+  //Calling setCounter auto function from useState
+  function handleIncrease() {
+    setCounter(counter + 1);
+  }
+  //calling setCounter auto function from useState
+  function handleDecrease() {
+    setCounter(counter - 1);
+  }
+
+ 
+
+  return (
+    <div className="counterDiv">
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+
+      <h1>{counter}</h1>
+    </div>
+  );
+}
